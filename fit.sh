faa5646ac61c6e22640f8ccfa7b5db182bb103aa
Sasha Mazurov <alexander.mazurov@gmail.com>
2014-04-09 07:38:18 +0200

    Change script location TODO: do it universal

--- fit.sh
+++ fit.sh
@@ -4,7 +4,7 @@ sleep $1
 ssh -x lxplus -t "source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/prod"\
 "/InstallArea/scripts/LbLogin.sh;"\
 ". /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v7r8p2"\
-"/InstallArea/scripts/SetupProject.sh Bender;"\
-"cd ~/thesis-flat;"\
-"~/thesis-flat/fit.py --decay=$2 --year=$3 --ptbegin=$4 --ptend=$5 --profile=$6;"\
+"/InstallArea/scripts/SetupProject.sh Bender v22r11;"\
+"cd ~/thesis-code;"\
+"~/thesis-code/fit.py --decay=$2 --year=$3 --ptbegin=$4 --ptend=$5 --profile=$6;"\
 "sleep 20"
